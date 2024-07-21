a5c2c6087cbc92dffcfc9b93d5ce2e32462f9a5f
JeffersonJesusDev <jefferson3680@hotmail.com>
2024-07-21 15:38:03 -0300

    Atualização

--- src/main/java/br/com/desafiopicpay/infra/AppConfig.java
+++ src/main/java/br/com/desafiopicpay/infra/AppConfig.java
@@ -1,9 +1,12 @@
 package br.com.desafiopicpay.infra;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-public class AppCondfig {
+@Configuration
+public class AppConfig {
+
     @Bean
     public RestTemplate restTemplate(){
         return new RestTemplate();
